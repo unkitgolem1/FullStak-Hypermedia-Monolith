b6cf1d26d911241bf3208c4749860eb6da1c0fc7
Daniel <danielmariestrada@gmail.com>
2026-06-27 02:23:41 -0400

    Comented code

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
-#from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from routes import router as landing_web
 from routes import adaptador_supabase as db
-app=FastAPI()
-#
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     print("Estableciendo conexion con la db")
-#     await db.conectar()
-#     yield
-#     print("Ya terminamos de usar la db, desconectando")
-#     await db.desconectar()
-#
-#
-# app = FastAPI(lifespan=lifespan)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Estableciendo conexion con la db")
+    await db.conectar()
+    yield
+    print("Ya terminamos de usar la db, desconectando")
+    await db.desconectar()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(landing_web)
